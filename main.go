db52ca2bdf743f82981e9cca592793865d41a432
Fernando Horas <102867453+fch-bsp@users.noreply.github.com>
2022-10-06 15:10:27 -0300

    v6

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "net/http"
 // Expondo menssagem na porta 8080 (Hello BSPCLOUD)
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<h1>Flow do Mateus V5<h1>"))
+		w.Write([]byte("<h1>Mateus viado V6<h1>"))
 	})
 	http.ListenAndServe(":8080", nil)
 
